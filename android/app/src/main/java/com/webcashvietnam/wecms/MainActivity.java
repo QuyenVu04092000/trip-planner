702a73e4561362c266ad17192a2c2ec4d5cc8d6b
quyenvu <quyenvu.uzumaki@gmail.com>
2026-08-05 09:33:59 +0700

    fix: ios app

---
+++ android/app/src/main/java/com/webcashvietnam/wecms/MainActivity.java
@@ -0,0 +1,5 @@
+package com.webcashvietnam.wecms;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.webcashglobal.tripmemo',
+  appId: 'com.webcashvietnam.wecms',
   appName: 'TripMemo',
   webDir: 'dist',
   ios: {
